ff447caa2058c0069106b866f3d7b452973c1b8b
Ryan Whitman <ryanawhitman@gmail.com>
2022-10-13 17:24:05 -0400

    Adhere to WP coding standards

--- src/helpers.php
+++ src/helpers.php
@@ -3,25 +3,24 @@
 /**
  * Determine if the given value is "blank."
  */
-function oyo_blank($value): bool
-{
-    if (is_null($value)) {
-        return true;
-    }
+function oyo_blank( $value ): bool {
+	if ( is_null( $value ) ) {
+		return true;
+	}
 
-    if (is_string($value)) {
-        return trim($value) === '';
-    }
+	if ( is_string( $value ) ) {
+		return trim( $value ) === '';
+	}
 
-    if (is_numeric($value) || is_bool($value)) {
-        return false;
-    }
+	if ( is_numeric( $value ) || is_bool( $value ) ) {
+		return false;
+	}
 
-    if ($value instanceof Countable) {
-        return count($value) === 0;
-    }
+	if ( $value instanceof Countable ) {
+		return count( $value ) === 0;
+	}
 
-    return empty($value);
+	return empty( $value );
 }
 
 /**
@@ -29,11 +28,11 @@ function oyo_blank($value): bool
  *
  * Usage: oyo_element_attr(<a href="example.com">, 'href') yields example.com
  */
-function oyo_element_attr(string $htmlEl, string $attr): ?string {
-    if (oyo_blank($attr)) {
-        throw new Exception("A valid attr must be provided.");
-    }
+function oyo_element_attr( string $html_el, string $attr ): ?string {
+	if ( oyo_blank( $attr ) ) {
+		throw new Exception( 'A valid attr must be provided.' );
+	}
 
-    preg_match("/{$attr}=\"(.+?)\"/", $htmlEl, $matches);
-    return $matches[1] ?? null;
+	preg_match( "/{$attr}=\"(.+?)\"/", $html_el, $matches );
+	return $matches[1] ?? null;
 }
