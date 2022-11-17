098c41c2d778367d837e31036d3c4bbb209d3106
Ryan Whitman <ryanawhitman@gmail.com>
2022-11-17 10:52:57 -0500

    Adding Whoops Error Handler

---
+++ src/ErrorHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oyova\WpSupport;
+
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PlainTextHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run as WhoopsRun;
+use Whoops\Util\Misc as WhoopsMisc;
+
+class ErrorHandler {
+
+	public function __construct() {
+		if (
+			defined( 'WP_DEBUG' ) &&
+			WP_DEBUG &&
+			(
+				! defined( 'WP_DEBUG_DISPLAY' ) ||
+				WP_DEBUG_DISPLAY
+			)
+		) {
+			$whoops = new WhoopsRun();
+			$whoops->pushHandler(
+				WhoopsMisc::isAjaxRequest() ?
+				new JsonResponseHandler() :
+				(
+					WhoopsMisc::isCommandLine() ?
+					new PlainTextHandler() :
+					new PrettyPageHandler()
+				)
+			);
+			$whoops->register();
+		}
+	}
+}
